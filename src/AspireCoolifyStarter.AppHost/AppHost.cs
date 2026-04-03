f6f53185d80071c93b60615b1f783b8d0e8e4a0f
Funs Janssen <funs.janssen@hotmail.com>
2026-04-03 21:10:15 +0200

    adds docker compose and github workflow

--- src/AspireCoolifyStarter.AppHost/AppHost.cs
+++ src/AspireCoolifyStarter.AppHost/AppHost.cs
@@ -1,5 +1,20 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddDockerComposeEnvironment("docker-compose")
+    .WithProperties(options =>
+    {
+        options.DefaultNetworkName = null;
+    })
+    .ConfigureComposeFile(options =>
+    {
+        options.Networks.Clear();
+        foreach (var service in options.Services)
+        {
+            service.Value.Networks.Clear();
+            service.Value.PullPolicy = "always";
+        }
+    });
+
 var apiService = builder.AddProject<Projects.AspireCoolifyStarter_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
